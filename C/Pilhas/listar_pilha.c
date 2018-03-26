30a2f01190112e878905d44f8138d36df5860b57
Exata0mente <exata0mente@gmail.com>
2018-03-26 18:00:33 -0300

    Corrigido o problema de apresentar mensagem de pilha não inicializada mesmo ela estando inicializada

--- C/Pilhas/listar_pilha.c
+++ C/Pilhas/listar_pilha.c
@@ -9,16 +9,17 @@ void listar_pilha(int *p1, int *p2, int topo1, int topo2){
 
     for(i = T-1; i >= 0;i--){
         
-        if(topo1 == -1){
-            printf("Pilha não inicalizada :(\n");
+        /*if(topo1 == -1){
+            printf("Considere inicializar... :(\t");
         }
         else
-            printf("Pilha Impar [%d] = %d%s \t\t\t", i, *(p1 + i), (i == topo1) ? " <-- TOPO" : "");
+        */    printf("Pilha Impar [%d] = %d%s \t\t\t", i, *(p1 + i), (i == topo1) ? " *" : "");
         
-        if(topo2 == -1){
-            printf("Pilha não inicalizada :(\n");
+        /*if(topo2 == -1){
+            printf("Considere inicializar... :(\n");
         }
         else
-            printf("Pilha Par [%d] = %d%s ", i, *(p2 + i), (i == topo2) ? " <-- TOPO\n" : "\n");
+        */    printf("Pilha Par [%d] = %d%s ", i, *(p2 + i), (i == topo2) ? " *\n" : "\n");
     }
+    printf("\nSendo * o topo da pilha\n");
 }
